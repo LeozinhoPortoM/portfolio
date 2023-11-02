329b9dcb853f8d0d5ba47658c9625a7ec442c88f
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-11-02 01:36:52 -0300

    Including created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
+
 import RoutesApp from "./routes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <RoutesApp />
+      <Footer />
     </BrowserRouter>
   );
 }
