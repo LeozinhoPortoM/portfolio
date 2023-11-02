3a541b6706f00906e7364affe2f94d2b931a056d
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-11-02 01:37:46 -0300

    including pages routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Service from "../pages/Service";
+import Portfolio from "../pages/Portfolio";
+import Contact from "../pages/Contact";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 
 function RoutesApp() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
