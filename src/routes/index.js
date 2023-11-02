5ce6c670fbc544677e0ddd5e7203755d61018a38
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-11-02 00:33:39 -0300

    Routes config

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,3 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+
+function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+    </Routes>
+  );
+}
+
+export default RoutesApp;
